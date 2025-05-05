602b5bb0fe4f23193f1ea9f2f11afdffe1e98979
sohaila11 <106407384+sohaila11@users.noreply.github.com>
2025-05-05 16:55:17 +0300

    edit and delete redux functions

--- my-react-app/src/store/PostSlice.js
+++ my-react-app/src/store/PostSlice.js
@@ -1,8 +1,10 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { db } from "../FireBaseConfig";
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import { collection, getDocs, addDoc,deleteDoc,doc, updateDoc } from "firebase/firestore";
+
 
 export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+  
   const snapshot = await getDocs(collection(db, "posts"));
   return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() ,
     createdAt:doc.data().createdAt?.toDate().toISOString(),}));
@@ -12,7 +14,15 @@ export const addPost = createAsyncThunk("posts/addPost", async (newPost) => {
   await addDoc(collection(db, "posts"), newPost);
 });
 
+export const deletePost = createAsyncThunk("posts/deletePost",async (postId)=>{
+  await deleteDoc(doc(db,"posts",postId));
+  return postId;
+});
 
+export const editPost=createAsyncThunk("posts/edit",async({postId,updatedPost})=>{
+  await updateDoc(doc(db,"posts",postId),updatedPost);
+  return {postId,updatedPost}
+})
 const postSlice = createSlice({
   name: "posts",
   initialState: {list:[]},
@@ -20,6 +30,15 @@ const postSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(fetchPosts.fulfilled, (state, action) => {
       state.list = action.payload;
+    });
+    builder.addCase(deletePost.fulfilled,(state,action)=>{
+      state.list=state.list.filter(post => post.id !== action.payload);
+    });
+    builder.addCase(editPost.fulfilled,(state,action)=>{
+      const index = state.list.findIndex(post =>post.id === action.payload.postId);
+      if(index !== -1){
+      state.list[index]={...state.list[index],...action.payload.updatedPost};
+      }
     })
   }
 });
