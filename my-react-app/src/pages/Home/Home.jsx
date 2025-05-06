a215e384ea6e7a499794248bec656afb935991f0
sohaila11 <106407384+sohaila11@users.noreply.github.com>
2025-05-06 15:36:04 +0300

    Update Home.jsx

--- my-react-app/src/pages/Home/Home.jsx
+++ my-react-app/src/pages/Home/Home.jsx
@@ -2,8 +2,8 @@ import "../Home/Home.css"
 function Home(){
     return(
     <div className="Home">
-    <h1 className="title"> Blog</h1>
-    <p className="content">Stay informed with the latest trends, innovations, and developments from various fields! <br/> Whether it's technology, science, business, or culture, you'll find fresh updates and insights right here.</p>
+    <h1 className="title"> Food Blog</h1>
+    <p className="content">Stay updated on the latest food trends, innovations, and flavors! <br/>From new ingredients to cooking tips, there’s always something fresh to explore.</p>
     <h2 className="JoinNow"> Join Our Community Now!! </h2>
     </div>
     )
